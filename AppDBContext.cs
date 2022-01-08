238b8d3a0df460a61b43b783dd9307a436e41b2c
Artem Huk <91683595+ArtemHuk@users.noreply.github.com>
2022-01-08 18:38:40 +0200

    Task15

---
+++ AppDBContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Task15.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Task15
+{
+    public class AppDBContext:DbContext
+    {
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Cities.db;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Country>().Navigation(x => x.Cities).AutoInclude();
+            modelBuilder.Entity<City>().Navigation(x => x.Country).AutoInclude();
+        }
+
+        public AppDBContext() : base() { }
+    }
+}
---
+++ Models/Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Task15.Models
+{
+    public class Country
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<City> Cities { get; set; }
+
+        public Country()
+        {
+            Cities = new HashSet<City>();
+        }
+    }
+}
