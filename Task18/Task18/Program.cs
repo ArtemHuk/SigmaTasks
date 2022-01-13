6c8bce38dde0df6923e0fdf96907047b9013fc24
Artem Huk <91683595+ArtemHuk@users.noreply.github.com>
2022-01-13 21:57:17 +0200

    Add files via upload

---
+++ Task18/Task18/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Task18
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] data = { 3, 54, 4, 64, 61 };
+
+                data.QuickSort();
+
+                foreach (var item in data)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            
+            
+        }
+    }
+}
